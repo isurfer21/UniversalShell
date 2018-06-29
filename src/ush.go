42b155be75e7de32aae1898c764cc57ae26cca00
Abhishek Kumar <kumarab@hcl.com>
2018-06-29 17:03:45 +0530

    Finalised app root help

--- src/ush.go
+++ src/ush.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
+	"strconv"
 
 	"github.com/mkideal/cli"
+)
+
+var (
+	appVersion = "0.0.1"
+	appLicense = "MIT License"
+
+	appLicenseDetail = "A short and simple permissive license with conditions only requiring \npreservation of copyright and license notices. Licensed works, \nmodifications, and larger works may be distributed under different \nterms and without source code."
+	appCopyrightYear = 2018
 )
 
 func main() {
@@ -19,25 +29,34 @@ func main() {
 
 var help = cli.HelpCommand("display help information")
 
-// root command
 type rootT struct {
 	cli.Helper
-	Name string `cli:"name" usage:"your name"`
+	Version bool `cli:"v,version" usage:"show version number and quit"`
+	License bool `cli:"l,license" usage:"show license and quit"`
 }
 
 var root = &cli.Command{
-	Desc: "this is root command",
+	Desc: "Welcome to UniversalShell (ush)",
 	// Argv is a factory function of argument object
 	// ctx.Argv() is if Command.Argv == nil or Command.Argv() is nil
 	Argv: func() interface{} { return new(rootT) },
 	Fn: func(ctx *cli.Context) error {
 		argv := ctx.Argv().(*rootT)
-		ctx.String("Hello, root command, I am %s\n", argv.Name)
+		if argv.Version {
+			currentYear := time.Now().UTC().Year()
+			copyrightDuration := strconv.Itoa(appCopyrightYear)
+			if currentYear > appCopyrightYear {
+				copyrightDuration += "-" + strconv.Itoa(currentYear)
+			}
+			ctx.String("UniversalShell (ush)    version %s\nCopyright (c) %s Abhishek Kumar.\nIt is licensed under the '%s'.\n\n", appVersion, copyrightDuration, appLicense)
+		}
+		if argv.License {
+			ctx.String("The 'UniversalShell (ush)' is licensed under the '%s'\n\n%s\n\n", appLicense, appLicenseDetail)
+		}
 		return nil
 	},
 }
 
-// child command
 type childT struct {
 	cli.Helper
 	Name string `cli:"name" usage:"your name"`
