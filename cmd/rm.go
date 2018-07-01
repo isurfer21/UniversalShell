0eac66cbb2c861807b577f1b958937a834265a6b
Abhishek Kumar <akbittu@gmail.com>
2018-07-01 22:19:29 +0530

    Implemented basic functionality of rm command

--- cmd/rm.go
+++ cmd/rm.go
@@ -6,57 +6,47 @@ This work is licensed under the 'MIT License'.
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// rmCmd represents the rm command
-var rmCmd = &cobra.Command{
-	Use:   "rm",
-	Short: "Remove files and directories",
-	Long: ` 
+const (
+	i18nRmCmdTitle  = "Remove files and directories"
+	i18nRmCmdDetail = `
 Remove files and directories
 
 It attempts to remove the non-directory type files specified on the 
 command line. If the permissions of the file do not permit writing,
 and the standard input device is a terminal, the user is prompted
 (on the standard error output) for confirmation.
-`,
-	Args: cobra.ExactArgs(1),
+`
+)
+
+type rmFlag struct {
+}
+
+var rmFlg cpFlag
+
+// rmCmd represents the rm command
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: i18nRmCmdTitle,
+	Long:  i18nRmCmdDetail,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 1 {
-			dir := strings.TrimSuffix(filepath.Base(args[0]), filepath.Ext(args[0]))
-			dir = filepath.Join(os.TempDir(), dir)
-			dirs := filepath.Join(dir, `tmpdir`)
-			err := os.MkdirAll(dirs, 0777)
-			if err != nil {
+			path, err := os.Stat(args[0])
+			if err == nil {
+				if path.IsDir() {
+					os.RemoveAll(args[0])
+				} else {
+					os.Remove(args[0])
+				}
+			} else {
 				log.Fatal(err)
-				os.Exit(1)
-			}
-			file := filepath.Join(dir, `tmpfile`)
-			f, err := os.Create(file)
-			if err != nil {
-				log.Fatal(err)
-				os.Exit(1)
-			}
-			f.Close()
-			file = filepath.Join(dirs, `tmpfile`)
-			f, err = os.Create(file)
-			if err != nil {
-				log.Fatal(err)
-				os.Exit(1)
-			}
-			f.Close()
-
-			err = removeContents(dir)
-			if err != nil {
-				log.Fatal(err)
-				os.Exit(1)
 			}
 		}
 	},
@@ -72,23 +62,4 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command	is called directly, e.g.:
 	// rmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func removeContents(dir string) error {
-	d, err := os.Open(dir)
-	if err != nil {
-		return err
-	}
-	defer d.Close()
-	names, err := d.Readdirnames(-1)
-	if err != nil {
-		return err
-	}
-	for _, name := range names {
-		err = os.RemoveAll(filepath.Join(dir, name))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
