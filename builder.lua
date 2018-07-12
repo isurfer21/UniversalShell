96fe0a8ca7f37e0eba5b362a7c81f21d138a3817
Abhishek Kumar <kumarab@hcl.com>
2018-07-12 13:04:18 +0530

    Builder is ready

--- builder.lua
+++ builder.lua
@@ -61,7 +61,7 @@ function xbuilds()
 end
 
 function release()
-  print("Create distributable packages")
+  print("# Create distributable packages")
 
   local rootDir = ush('pwd')
   local srcDir = rootDir
@@ -76,43 +76,92 @@ function release()
     ushfp('mkdir "%s"', pubDir)
   end
 
-  print("- Copying icons")
+  print("Copying icons")
   ushfp('cp "%s/img/appicon.ico" "%s/appicon.ico"', srcDir, binDir)
   ushfp('cp "%s/img/appicon.icns" "%s/appicon.icns"', srcDir, binDir)
   
-  print("- Copying doc files")
+  print("Copying doc files")
   ushfp('cp "%s/LICENSE" "%s/LICENSE"', rootDir, binDir)
   ushfp('cp "%s/README.md" "%s/README.md"', rootDir, binDir)
 
-  print("Create compressed binary distributable files")
+  print("# Create compressed binary distributable files")
 
   if contains(arg, '-win') then 
-    print('  Publishing release for Windows')
-    ushfp('rm "%s/Ush_windows_x86-64.zip"', pubDir)
-    local tmpDir = pubDir.."/Ush_windows_x86-64"
+    print('Publishing release for Windows')
+    if ushf('ls -e "%s/Ush_windows_x86-64.zip"', pubDir) == 'true' then
+      print('Delete last release build for Windows')
+      ushfp('rm "%s/Ush_windows_x86-64.zip"', pubDir)
+    end 
+    local tmpDir = pubDir.."/ush_windows_x86-64"
     ushfp('mkdir "%s"', tmpDir)
     ushfp('cp "%s/LICENSE" "%s/LICENSE"', binDir, tmpDir)
     ushfp('cp "%s/README.md" "%s/README.md"', binDir, tmpDir)
     ushfp('cp "%s/appicon.ico" "%s/appicon.ico"', binDir, tmpDir)
     ushfp('mkdir "%s/bin"', tmpDir)
-    ushfp('cp "%s/Ush_windows_386.exe" "%s/bin/Ush_windows_386.exe"', binDir, tmpDir)
-    ushfp('cp "%s/Ush_windows_amd64.exe" "%s/bin/Ush_windows_amd64.exe"', binDir, tmpDir)
-    shfp('zip -r -X "%s/Ush_windows_x86-64.zip" .', pubDir)
-    -- ushfp('rm -r -f "%s"', tmpDir)
+    ushfp('cp "%s/ush_windows_386.exe" "%s/bin/ush_windows_32bit.exe"', binDir, tmpDir)
+    ushfp('cp "%s/ush_windows_amd64.exe" "%s/bin/ush_windows_64bit.exe"', binDir, tmpDir)
+    ushfp('zip "%s/Ush_windows_x86-64.zip" "%s/"', pubDir, tmpDir)
+    ushfp('rm -r -f "%s"', tmpDir)
     if contains(arg, '-rmb') then 
-      print("- Delete all builds for Windows")
-      ushfp('rm -r -f "%s/Ush_windows_386.exe"', binDir)
-      ushfp('rm -r -f "%s/Ush_windows_amd64.exe"', binDir)
+      print("Delete all builds for Windows")
+      ushfp('rm -r -f "%s/ush_windows_386.exe"', binDir)
+      ushfp('rm -r -f "%s/ush_windows_amd64.exe"', binDir)
     end
-    print("  Published.")
+    print("Published")
   end
 
   if contains(arg, '-mac') then 
-    print('  Publishing release for MacOSX')
+    print('Publishing release for MacOSX')
+    if ushf('ls -e "%s/Ush_macos_x86-64.zip"', pubDir) == 'true' then
+      print('Delete last release build for MacOSX')
+      ushfp('rm "%s/Ush_macos_x86-64.zip"', pubDir)
+    end 
+    local tmpDir = pubDir.."/ush_macos_x86-64"
+    ushfp('mkdir "%s"', tmpDir)
+    ushfp('cp "%s/LICENSE" "%s/LICENSE"', binDir, tmpDir)
+    ushfp('cp "%s/README.md" "%s/README.md"', binDir, tmpDir)
+    ushfp('cp "%s/appicon.ico" "%s/appicon.ico"', binDir, tmpDir)
+    ushfp('mkdir "%s/bin"', tmpDir)
+    ushfp('cp "%s/ush_darwin_386" "%s/bin/ush_darwin_32bit"', binDir, tmpDir)
+    ushfp('cp "%s/ush_darwin_amd64" "%s/bin/ush_darwin_64bit"', binDir, tmpDir)
+    ushfp('zip "%s/Ush_macos_x86-64.zip" "%s/"', pubDir, tmpDir)
+    ushfp('rm -r -f "%s"', tmpDir)
+    if contains(arg, '-rmb') then 
+      print("Delete all builds for MacOSX")
+      ushfp('rm -r -f "%s/ush_darwin_386"', binDir)
+      ushfp('rm -r -f "%s/ush_darwin_amd64"', binDir)
+    end
+    print("Published")
   end
 
   if contains(arg, '-nix') then 
-    print('  Publishing release for Linux')
+    print('Publishing release for Linux')
+    if ushf('ls -e "%s/Ush_linux_x86-64.zip"', pubDir) == 'true' then
+      print('Delete last release build for Linux')
+      ushfp('rm "%s/Ush_linux_x86-64.zip"', pubDir)
+    end 
+    local tmpDir = pubDir.."/ush_linux_x86-64"
+    ushfp('mkdir "%s"', tmpDir)
+    ushfp('cp "%s/LICENSE" "%s/LICENSE"', binDir, tmpDir)
+    ushfp('cp "%s/README.md" "%s/README.md"', binDir, tmpDir)
+    ushfp('cp "%s/appicon.ico" "%s/appicon.ico"', binDir, tmpDir)
+    ushfp('mkdir "%s/bin"', tmpDir)
+    ushfp('cp "%s/ush_linux_386" "%s/bin/ush_linux_32bit"', binDir, tmpDir)
+    ushfp('cp "%s/ush_linux_amd64" "%s/bin/ush_linux_64bit"', binDir, tmpDir)
+    if ushf('ls -e "%s/ush_linux_arm"', binDir) == 'true' then
+      ushfp('cp "%s/ush_linux_arm" "%s/bin/ush_linux_arm"', binDir, tmpDir)
+    end
+    ushfp('zip "%s/Ush_linux_x86-64.zip" "%s/"', pubDir, tmpDir)
+    ushfp('rm -r -f "%s"', tmpDir)
+    if contains(arg, '-rmb') then 
+      print("Delete all builds for Linux")
+      ushfp('rm -r -f "%s/ush_linux_386"', binDir)
+      ushfp('rm -r -f "%s/ush_linux_amd64"', binDir)
+      if ushf('ls -e "%s/ush_linux_arm"', binDir) == 'true' then
+        ushfp('rm -r -f "%s/ush_linux_arm"', binDir)
+      end
+    end
+    print("Published")
   end
 
   print("Done!")
